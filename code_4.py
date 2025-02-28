1daeb5e8a2e120ac062aea60dae6d2901c646d01
djfairbanks-utep <dallinfair96@gmail.com>
2025-02-28 13:56:51 -0700

    Update code_4.py

--- code_4.py
+++ code_4.py
@@ -1,9 +1,12 @@
 import csv
 import pandas as pd
 big_mac_file = './big-mac-full-index.csv'
+df = pd.read_csv(big_mac_file)
 
 def get_big_mac_price_by_year(year,country_code):
-    pass # Remove this line and code your function
+    query = df[(df['iso_a3'].str.lower() == country_code) & (df['date'].str.startswith(str(year)))]
+    rounda = round(query['dollar_price'].mean(),2)
+    return rounda
 
 def get_big_mac_price_by_country(country_code):
     pass # Remove this line and code your function
@@ -15,4 +18,7 @@ def get_the_most_expensive_big_mac_price_by_year(year):
     pass # Remove this line and code your function
 
 if __name__ == "__main__":
-    pass # Remove this line and code your user interface
+    # print(type(df.loc[0]["date"]))
+    # df["date"] = pd.todatetime(df["date"])
+    
+    print(get_big_mac_price_by_year(2010,"usa"))
