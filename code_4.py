0f0f3a5fc66f4393caae17b3cd4ffb27853b0638
djfairbanks-utep <dallinfair96@gmail.com>
2025-03-01 18:35:28 -0700

    Update code_4.py

--- code_4.py
+++ code_4.py
@@ -11,72 +11,29 @@ df = pd.read_csv('big-mac-full-index.csv')
 #        'GBP_adjusted', 'JPY_adjusted', 'CNY_adjusted']
 
 def get_big_mac_price_by_year(year,country_code):
-
-    country_codes = ['arg','aus','bra','can','che','chl','chn',
-                    'cze','dnk','euz','gbr','hkg','hun','idn',
-                    'isr','jpn','kor','mex','mys','nzl','pol',
-                    'rus','sgp','swe','tha','twn','usa','zaf',
-                    'are','aze','bhr','col','cri','egy','gtm',
-                    'hon','hrv','jor','kwt','lbn','lka','mda',
-                    'nic','nor','omn','pak','per','phl','qat',
-                    'rou','sau','tur','ury','ven','vnm']
-    if country_code.lower() in country_codes: 
-        query_data = df[(df['iso_a3'].str.lower() == country_code.lower()) 
-                        & 
-                        (df['date'].str.startswith(str(year)))]
-    
-        round_a = round(query_data['dollar_price'].mean(),2)
-
-    return round_a
+    query = f"date >= '{year}-01-01' and date < '{year + 1}-01-01' and iso_a3 == '{country_code.upper()}'"
+    q_df = df.query(query)
+    return round(q_df["dollar_price"].mean(), 2)
 
 def get_big_mac_price_by_country(country_code):
-    
-    country_codes = ['arg','aus','bra','can','che','chl','chn',
-                    'cze','dnk','euz','gbr','hkg','hun','idn',
-                    'isr','jpn','kor','mex','mys','nzl','pol',
-                    'rus','sgp','swe','tha','twn','usa','zaf',
-                    'are','aze','bhr','col','cri','egy','gtm',
-                    'hon','hrv','jor','kwt','lbn','lka','mda',
-                    'nic','nor','omn','pak','per','phl','qat',
-                    'rou','sau','tur','ury','ven','vnm']
-    if country_code.lower() in country_codes: 
-        query_data = df[(df['iso_a3'].str.lower() == country_code.lower())]
-    
-        round_b = round(query_data['dollar_price'].mean(),2)
-
-    return round_b
+    query = f"iso_a3 == '{country_code.upper()}'"
+    q_df = df.query(query)
+    return round(q_df["dollar_price"].mean(), 2)
 
 def get_the_cheapest_big_mac_price_by_year(year):
-    
-    years = df[df['date'].str.startswith(str(year))]
-    price_c = years['dollar_price'].min() 
-    link = years.query('dollar_price == @price_c')
-
-    name = link['name'].iloc[0]
-    country_name = link['iso_a3'].iloc[0].upper() 
-
-    return f"{name}({country_name}): ${price_c:.2f}" 
+    query = f"date >= '{year}-01-01' and date < '{year + 1}-01-01'"
+    q_df = df.query(query)
+    min_row = q_df.loc[q_df["dollar_price"].idxmin()]
+    return f"{min_row["name"]}({min_row["iso_a3"]}): {round(min_row["dollar_price"],2)}"
 
 def get_the_most_expensive_big_mac_price_by_year(year):
-
-    years = df[df['date'].str.startswith(str(year))]
-    price_d = years['dollar_price'].max() 
-    link = years.query('dollar_price == @price_d')
-
-    name = link['name'].iloc[0]
-    country_name = link['iso_a3'].iloc[0].upper() 
-
-    return f"{name}({country_name}): ${price_d:.2f}" 
+    query = f"date >= '{year}-01-01' and date < '{year + 1}-01-01'"
+    q_df = df.query(query)
+    max_row = q_df.loc[q_df["dollar_price"].idxmax()]
+    return f"{max_row["name"]}({max_row["iso_a3"]}): {round(max_row["dollar_price"],2)}"
 
 if __name__ == "__main__":
-    result_a = get_big_mac_price_by_year(2009,"mex")
-    print(result_a)
-    
-    result_b = get_big_mac_price_by_country("mex")
-    print(result_b)
-    
-    result_c = get_the_cheapest_big_mac_price_by_year(2008)
-    print(result_c)
-    
-    result_d = get_the_most_expensive_big_mac_price_by_year(2003)
-    print(result_d)
+    print(get_big_mac_price_by_year(2012,'arg'))
+    print(get_big_mac_price_by_country('arg'))
+    print(get_the_cheapest_big_mac_price_by_year(2008))
+    print(get_the_most_expensive_big_mac_price_by_year(2003))
